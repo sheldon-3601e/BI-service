b7ed31f5c217df63c861a9d54ab6a868ab99cb6c
sheldon <liu_2648349896@126.com>
2024-01-29 17:58:11 +0800

    MQ功能重构完成

--- src/main/java/com/sheldon/springbootinit/bizmq/WaitingMsgConsumer.java
+++ src/main/java/com/sheldon/springbootinit/bizmq/WaitingMsgConsumer.java
@@ -1,6 +1,10 @@
 package com.sheldon.springbootinit.bizmq;
 
 import com.rabbitmq.client.Channel;
+import com.sheldon.springbootinit.common.ErrorCode;
+import com.sheldon.springbootinit.exception.BusinessException;
+import com.sheldon.springbootinit.model.entity.Chart;
+import com.sheldon.springbootinit.model.enums.ChartStatueEnum;
 import com.sheldon.springbootinit.service.ChartInfoService;
 import com.sheldon.springbootinit.service.ChartService;
 import lombok.extern.slf4j.Slf4j;
@@ -35,39 +39,19 @@ public class WaitingMsgConsumer {
 
     @RabbitListener(queues = {BiMqConstant.QUEUE_WAITING_NAME}, ackMode = "MANUAL")
     public void sendMessage(String message, Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) Long deliveryTag) {
-        log.info("接收等待分析图表信息：{}", message);
-        if (!StringUtils.isBlank(message)){
-            if ("succeed".equals(message)) {
-                log.info("succeed");
-                try {
-                    msgProducer.sendSucceedMsg(message);
-                    channel.basicAck(deliveryTag, false);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            } else {
-                try {
-                    log.error("failed");
-                    channel.basicNack(deliveryTag, false, false);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return;
-   /*     try {
+        log.info("MQ:接收等待分析图表信息：{}", message);
+        try {
             // 校验消息
             if (StringUtils.isBlank(message)) {
                 // 如果更新失败，拒绝当前消息，让消息重新进入队列
-                log.error("消息确认失败：{}", "message is blank");
-                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "消息为空");
+                log.error("消息确认失败：{}", "");
+                channel.basicNack(deliveryTag, false, true);
             }
             Long chartId = Long.parseLong(message);
             Chart chart = chartService.getById(chartId);
             if (chart == null) {
-                log.error("消息确认失败：{}", "chart is null");
+                log.error("消息确认失败：{}", "图表不存在");
                 channel.basicNack(deliveryTag, false, false);
-                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "图表不存在");
             }
 
             Chart updateChart = new Chart();
@@ -77,13 +61,14 @@ public class WaitingMsgConsumer {
             if (!b) {
                 // 如果更新图表执行中状态失败，拒绝消息并处理图表更新错误
                 log.error("消息确认失败：{}", "更新图表执行中状态失败");
-                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "更新图表执行中状态失败");
+                channel.basicNack(deliveryTag, false, false);
             }
             // 分析图表数据，并保存到数据库
             chartInfoService.genderChartInfo(chart);
 
             // 消息确认
             channel.basicAck(deliveryTag, false);
+            msgProducer.sendSucceedMsg(message);
         } catch (Exception e) {
             log.error("消息确认失败：{}", e.getMessage());
             try {
@@ -91,7 +76,7 @@ public class WaitingMsgConsumer {
             } catch (IOException ex) {
                 log.error("消息拒绝失败：{}", ex.getMessage());
             }
-        }*/
+        }
     }
 
 }
